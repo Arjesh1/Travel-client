5105b79e7a1b5ce1e59faa96c5bba0f8fb13e9eb
Arjesh <arjesh.khadka2@gmail.com>
2024-01-03 19:05:07 +1100

    feat: styled-components installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-
+import styled from 'styled-components';
 import './App.css';
+
+const MainWrapper = styled.div`
+  color: #BF4F74;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #BF4F74;
+  border-radius: 3px;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <MainWrapper>
       <p className="text-3xl font-bold underline ">
       Welcome to TravelVerse
     </p>
-    </div>
+    </MainWrapper>
   );
 }
 
